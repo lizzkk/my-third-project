c4a83a2a6af0565727c47fc15231ffad13877956
lizzkk <osiryaevaelizaveta@gmail.com>
2024-11-21 13:54:14 +0900

    finish

--- NewPract/Program.cs
+++ NewPract/Program.cs
@@ -6,22 +6,21 @@ namespace NewPract
     {
         static void Main(string[] args)
         {
-            Class1 class1 = new Class1();
+            MyLib.Math Math = new MyLib.Math();
 
-            class1.Hello();
-            class1.Number(52.78);
+            Math.Hello();
+            Math.Number(52.78);
 
             int num1 = 9;
-            int num2 = 4;
-            int num3 = 0;
+            int num2 = 6;
 
-            int sum = class1.Add(num1, num2);
+            int sum = Math.Add(num1, num2);
             Console.WriteLine($"Сумма {num1} и {num2} равна: {sum}");
 
-            bool isEven = class1.IsEven(num1);
+            bool isEven = Math.IsEven(num1);
             Console.WriteLine($"{num1} {(isEven ? "четное" : "нечетное")} число");
 
-            string massage = class1.Message();
+            string massage = Math.Message();
             Console.WriteLine(massage);
         }
     }
