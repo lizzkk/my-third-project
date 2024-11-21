363a015b1c160ab63dd6832231b1f65242353ee9
lizzkk <osiryaevaelizaveta@gmail.com>
2024-11-21 13:32:08 +0900

    Добавьте файлы проекта.

---
+++ MyLib/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace MyLib
+{
+    public class Class1
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Привет, мир))");
+        }
+        public void Number(double number) 
+        {
+            Console.WriteLine($"Вы ввели number: {number}");
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+        public string Message()
+        {
+            return "Hello из библиотеки!";
+        }
+    }
+}
---
+++ NewPract/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyLib;
+
+namespace NewPract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            class1.Hello();
+            class1.Number(52.78);
+
+            int num1 = 9;
+            int num2 = 4;
+            int num3 = 0;
+
+            int sum = class1.Add(num1, num2);
+            Console.WriteLine($"Сумма {num1} и {num2} равна: {sum}");
+
+            bool isEven = class1.IsEven(num1);
+            Console.WriteLine($"{num1} {(isEven ? "четное" : "нечетное")} число");
+
+            string massage = class1.Message();
+            Console.WriteLine(massage);
+        }
+    }
+}
